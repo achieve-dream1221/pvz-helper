0d615aee446b70d93aa0c195ef3f6b90f5f2bef4
achieve_dream <wulei7217@gmail.com>
2024-08-03 10:30:51 +0800

    增加图形化界面

---
+++ build.rs
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Be Wilson <be.wilson@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+// ANCHOR: book_cargo_executable_build_rs
+use cxx_qt_build::{CxxQtBuilder, QmlModule};
+
+fn main() {
+    CxxQtBuilder::new()
+        // Link Qt's Network library
+        // - Qt Core is always linked
+        // - Qt Gui is linked by enabling the qt_gui Cargo feature (default).
+        // - Qt Qml is linked by enabling the qt_qml Cargo feature (default).
+        // - Qt Qml requires linking Qt Network on macOS
+        .qt_module("Network")
+        .qml_module(QmlModule {
+            uri: "com.kdab.cxx_qt.demo",
+            rust_files: &["src/cxxqt_object.rs"],
+            qml_files: &["qml/main.qml"],
+            ..Default::default()
+        })
+        .with_opts(cxx_qt_lib_headers::write_headers())
+        .build();
+}
+// ANCHOR_END: book_cargo_executable_build_rs
